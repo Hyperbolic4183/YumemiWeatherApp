99e75935ffde0a1536417dce77d4c0af43369721
Amane Otsuka <a_otsuka@yumemi.co.jp>
2021-06-11 11:47:25 +0900

    YumemiWeather.fetchWeatherから渡される情報を所有する構造体を用意した。

---
+++ Yumemi-ios-training/Model/WeatherResponse.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherResponse.swift
+//  Yumemi-ios-training
+//
+//  Created by 大塚 周 on 2021/06/11.
+//
+import Foundation
+struct WeatherResponse {
+    let weather: String
+    let minTemp: Int
+    let maxTemp: Int
+    let date: Date
+}

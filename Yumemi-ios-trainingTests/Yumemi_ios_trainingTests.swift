de3e2a4109b55d30a65a4361f89cad862a2a968d
Amane Otsuka <a_otsuka@yumemi.co.jp>
2021-06-23 10:03:11 +0900

    テスト用の構造体を用意し、任意のstubを生成できるようにした。

--- Yumemi-ios-trainingTests/Yumemi_ios_trainingTests.swift
+++ Yumemi-ios-trainingTests/Yumemi_ios_trainingTests.swift
@@ -8,16 +8,23 @@
 import XCTest
 @testable import Yumemi_ios_training
 
+struct TestWeatherModel: Fetchable {
+    let weatherInformation: WeatherInformation
+    init(weatherInformation: WeatherInformation) {
+        self.weatherInformation = weatherInformation
+    }
+    func fetchYumemiWeather() -> Result<WeatherInformation, WeatherAppError> {
+        .success(weatherInformation)
+    }
+}
+
 class Yumemi_ios_trainingTests: XCTestCase {
     
     func test_天気予報がsunnyだったときに画面に晴れ画像が表示される() {
+        let sunnyWeatherInformation = WeatherInformation(weather: .sunny, minTemperature: "0", maxTemperature: "0")
+        let sunnyModel = TestWeatherModel(weatherInformation: sunnyWeatherInformation)
         
-        struct Sunny: Fetchable {
-            func fetchYumemiWeather() -> Result<WeatherInformation, WeatherAppError> {
-                .success(WeatherInformation(weather: .sunny, minTemperature: "0", maxTemperature: "0"))
-            }
-        }
-        let viewController = WeatherViewController(model: Sunny())
+        let viewController = WeatherViewController(model: sunnyModel)
         let view = viewController.weatherView
         let imageView = view.weatherImageView
         let reloadButton = view.reloadButton
@@ -27,13 +34,10 @@ class Yumemi_ios_trainingTests: XCTestCase {
         XCTAssertEqual(imageView.image!, UIImage(named: "sunny")!)
     }
     func test_天気予報がcloudyだったときに画面に晴れ画像が表示される() {
+        let cloudyWeatherInformation = WeatherInformation(weather: .cloudy, minTemperature: "0", maxTemperature: "0")
         
-        struct Cloudy: Fetchable {
-            func fetchYumemiWeather() -> Result<WeatherInformation, WeatherAppError> {
-                .success(WeatherInformation(weather: .cloudy, minTemperature: "0", maxTemperature: "0"))
-            }
-        }
-        let viewController = WeatherViewController(model: Cloudy())
+        let cloudyModel = TestWeatherModel(weatherInformation: cloudyWeatherInformation)
+        let viewController = WeatherViewController(model: cloudyModel)
         let view = viewController.weatherView
         let imageView = view.weatherImageView
         let reloadButton = view.reloadButton
@@ -44,12 +48,9 @@ class Yumemi_ios_trainingTests: XCTestCase {
     }
     func test_天気予報がrainyだったときに画面に晴れ画像が表示される() {
         
-        struct Rainy: Fetchable {
-            func fetchYumemiWeather() -> Result<WeatherInformation, WeatherAppError> {
-                .success(WeatherInformation(weather: .rainy, minTemperature: "0", maxTemperature: "0"))
-            }
-        }
-        let viewController = WeatherViewController(model: Rainy())
+        let rainyWeatherInformation = WeatherInformation(weather: .rainy, minTemperature: "0", maxTemperature: "0")
+        let rainyModel = TestWeatherModel(weatherInformation: rainyWeatherInformation)
+        let viewController = WeatherViewController(model: rainyModel)
         let view = viewController.weatherView
         let imageView = view.weatherImageView
         let reloadButton = view.reloadButton
@@ -60,20 +61,11 @@ class Yumemi_ios_trainingTests: XCTestCase {
     }
     func test_最高気温がUILabelに反映される() {
         
-        struct MaxTemperature: Fetchable {
-            
-            let maxTemperature: String
-            
-            init(maxTemperature: String) {
-                self.maxTemperature = maxTemperature
-            }
-            func fetchYumemiWeather() -> Result<WeatherInformation, WeatherAppError> {
-                .success(WeatherInformation(weather: .sunny, minTemperature: "0", maxTemperature: maxTemperature))
-            }
-        }
-        
         let testingMaxTemperature = "40"
-        let viewController = WeatherViewController(model: MaxTemperature(maxTemperature: testingMaxTemperature))
+        let maxTemperatureWeatherInformation = WeatherInformation(weather: .sunny, minTemperature: "0", maxTemperature: testingMaxTemperature)
+        let maxTemperatureModel = TestWeatherModel(weatherInformation: maxTemperatureWeatherInformation)
+        
+        let viewController = WeatherViewController(model: maxTemperatureModel)
         let view = viewController.weatherView
         let maxTemperature = view.maxTemperatureLabel
         let reloadButton = view.reloadButton
@@ -84,21 +76,12 @@ class Yumemi_ios_trainingTests: XCTestCase {
     }
     
     func test_最低気温がUILabelに反映される() {
-        
-        struct MinTemperature: Fetchable {
-            
-            let minTemperature: String
-            
-            init(minTemperature: String) {
-                self.minTemperature = minTemperature
-            }
-            func fetchYumemiWeather() -> Result<WeatherInformation, WeatherAppError> {
-                .success(WeatherInformation(weather: .sunny, minTemperature: minTemperature, maxTemperature: "0"))
-            }
-        }
-        
+
         let testingMinTemperature = "-40"
-        let viewController = WeatherViewController(model: MinTemperature(minTemperature: testingMinTemperature))
+        let minTemperatureWeatherInformation = WeatherInformation(weather: .sunny, minTemperature: testingMinTemperature, maxTemperature: "0")
+        let minTemperatureModel = TestWeatherModel(weatherInformation: minTemperatureWeatherInformation)
+        
+        let viewController = WeatherViewController(model: minTemperatureModel)
         let view = viewController.weatherView
         let minTemperature = view.minTemperatureLabel
         let reloadButton = view.reloadButton

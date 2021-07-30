123f247f21fac87f18cfe1bf34c2566c4a63434e
Amane <60565914+Hyperbolic4183@users.noreply.github.com>
2021-07-30 22:19:18 +0900

    Update Yumemi-ios-training/Model/Fetcher.swift
    Co-authored-by: KAWASHIMA Yoshiyuki <ykawashi7@gmail.com>

--- Yumemi-ios-training/Model/Fetcher.swift
+++ Yumemi-ios-training/Model/Fetcher.swift
@@ -9,8 +9,8 @@ import YumemiWeather
 import Foundation
 
 protocol FetcherDelegate: AnyObject {
-    func fetcher(_ fetcher: Fetchable?, didFetch information: WeatherInformation)
-    func fetcher(_ fetcher: Fetchable?, didFailWithError error: WeatherAppError)
+    func fetch(_ fetchable: Fetchable?, didFetch information: WeatherInformation)
+    func fetch(_ fetchable: Fetchable?, didFailWithError error: WeatherAppError)
 }
 
 class Fetcher: Fetchable {

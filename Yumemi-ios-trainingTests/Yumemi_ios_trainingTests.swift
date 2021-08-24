62991c03eea30d9399828f9591711780f83c5313
Hyperbolic4183 <otsuka.amane@gmail.com>
2021-08-24 17:51:06 +0900

    fix 壊れていたテストを修正した
    テストの対象をWeatherView()のchangeDisplay()のみにした。

--- Yumemi-ios-trainingTests/Yumemi_ios_trainingTests.swift
+++ Yumemi-ios-trainingTests/Yumemi_ios_trainingTests.swift
@@ -8,87 +8,74 @@
 import XCTest
 @testable import Yumemi_ios_training
 
-struct TestWeatherModel: Fetchable {
-    let weatherInformation: WeatherInformation
-    init(weatherInformation: WeatherInformation) {
-        self.weatherInformation = weatherInformation
-    }
-    func fetchYumemiWeather() -> Result<WeatherInformation, WeatherAppError> {
-        .success(weatherInformation)
-    }
-}
-
 class Yumemi_ios_trainingTests: XCTestCase {
     
     func test_天気予報がsunnyだったときに画面に晴れ画像が表示される() {
         let sunnyWeatherInformation = WeatherInformation(weather: .sunny, minTemperature: "0", maxTemperature: "0")
-        let sunnyModel = TestWeatherModel(weatherInformation: sunnyWeatherInformation)
-        
-        let viewController = WeatherViewController(model: sunnyModel)
-        let view = viewController.weatherView
-        let imageView = view.weatherImageView
-        let reloadButton = view.reloadButton
-        
-        viewController.viewDidLoad()
-        reloadButton.sendActions(for: .touchUpInside)
-        XCTAssertEqual(imageView.image!, UIImage(named: "sunny")!)
+        let view = WeatherView()
+        view.changeDisplay(WeatherViewState(information: sunnyWeatherInformation))
+        let weatherImage = getImage(from: view)
+        XCTAssertEqual(weatherImage, UIImage(named: "sunny"))
     }
     func test_天気予報がcloudyだったときに画面に曇り画像が表示される() {
         let cloudyWeatherInformation = WeatherInformation(weather: .cloudy, minTemperature: "0", maxTemperature: "0")
-        let cloudyModel = TestWeatherModel(weatherInformation: cloudyWeatherInformation)
-        
-        let viewController = WeatherViewController(model: cloudyModel)
-        let view = viewController.weatherView
-        let imageView = view.weatherImageView
-        let reloadButton = view.reloadButton
-        
-        viewController.viewDidLoad()
-        reloadButton.sendActions(for: .touchUpInside)
-        XCTAssertEqual(imageView.image!, UIImage(named: "cloudy")!)
+        let view = WeatherView()
+        view.changeDisplay(WeatherViewState(information: cloudyWeatherInformation))
+        let weatherImage = getImage(from: view)
+        XCTAssertEqual(weatherImage, UIImage(named: "cloudy"))
     }
     func test_天気予報がrainyだったときに画面に雨画像が表示される() {
-        
         let rainyWeatherInformation = WeatherInformation(weather: .rainy, minTemperature: "0", maxTemperature: "0")
-        let rainyModel = TestWeatherModel(weatherInformation: rainyWeatherInformation)
-        
-        let viewController = WeatherViewController(model: rainyModel)
-        let view = viewController.weatherView
-        let imageView = view.weatherImageView
-        let reloadButton = view.reloadButton
-        
-        viewController.viewDidLoad()
-        reloadButton.sendActions(for: .touchUpInside)
-        XCTAssertEqual(imageView.image!, UIImage(named: "rainy")!)
+        let view = WeatherView()
+        view.changeDisplay(WeatherViewState(information: rainyWeatherInformation))
+        let weatherImage = getImage(from: view)
+        XCTAssertEqual(weatherImage, UIImage(named: "rainy"))
     }
     func test_最高気温がUILabelに反映される() {
-        
-        let testingMaxTemperature = "40"
-        let maxTemperatureWeatherInformation = WeatherInformation(weather: .sunny, minTemperature: "0", maxTemperature: testingMaxTemperature)
-        let maxTemperatureModel = TestWeatherModel(weatherInformation: maxTemperatureWeatherInformation)
-        
-        let viewController = WeatherViewController(model: maxTemperatureModel)
-        let view = viewController.weatherView
-        let maxTemperature = view.maxTemperatureLabel
-        let reloadButton = view.reloadButton
-        
-        viewController.viewDidLoad()
-        reloadButton.sendActions(for: .touchUpInside)
-        XCTAssertEqual(maxTemperature.text, testingMaxTemperature)
+        let testMaxTemperature = "40"
+        let view = WeatherView()
+        let minTemperatureWeatherInformation = WeatherInformation(weather: .sunny, minTemperature: "0", maxTemperature: testMaxTemperature)
+        view.changeDisplay(WeatherViewState(information: minTemperatureWeatherInformation))
+        let maxTemperatureLabel = getMaxTemperatureLabel(from: view)
+        XCTAssertEqual(maxTemperatureLabel.text, testMaxTemperature)
     }
     
     func test_最低気温がUILabelに反映される() {
-
-        let testingMinTemperature = "-40"
-        let minTemperatureWeatherInformation = WeatherInformation(weather: .sunny, minTemperature: testingMinTemperature, maxTemperature: "0")
-        let minTemperatureModel = TestWeatherModel(weatherInformation: minTemperatureWeatherInformation)
-        
-        let viewController = WeatherViewController(model: minTemperatureModel)
-        let view = viewController.weatherView
-        let minTemperature = view.minTemperatureLabel
-        let reloadButton = view.reloadButton
-        
-        viewController.viewDidLoad()
-        reloadButton.sendActions(for: .touchUpInside)
-        XCTAssertEqual(minTemperature.text, testingMinTemperature)
+        let testMinTemperature = "-40"
+        let view = WeatherView()
+        let minTemperatureWeatherInformation = WeatherInformation(weather: .sunny, minTemperature: testMinTemperature, maxTemperature: "0")
+        view.changeDisplay(WeatherViewState(information: minTemperatureWeatherInformation))
+        let minTemperatureLabel = getMinTemperatureLabel(from: view)
+        XCTAssertEqual(minTemperatureLabel.text, testMinTemperature)
+    }
+    
+    private func getStackView(from view: UIView) -> UIStackView {
+        view.subviews.first(where: { $0 is UIStackView})! as! UIStackView
+    }
+    
+    private func getImage(from view: WeatherView) -> UIImage? {
+        //WeatherView含まれるstackViewForImageViewAndLabelsを取得
+        let stackViewForImageViewAndLabels = getStackView(from: view)
+        let weatherImageView = stackViewForImageViewAndLabels.subviews.first(where: { $0 is UIImageView })! as! UIImageView
+        return weatherImageView.image
+    }
+    
+    private func getMaxTemperatureLabel(from view: WeatherView) -> UILabel {
+        //WeatherView含まれる2つのUIStackViewのうち、2番目に追加したstackViewForLabelsを取得
+        let stackViewForImageViewAndLabels = getStackView(from: view)
+        //stackViewForImageViewAndLabelsに含まれるstackViewForLabelsを取得
+        let stackViewForLabels = getStackView(from: stackViewForImageViewAndLabels)
+        //stackViewForLabelsに2番目に追加したUILabelを取得
+        let maxTemperatureLabel = stackViewForLabels.subviews[1] as! UILabel
+        return maxTemperatureLabel
+    }
+    private func getMinTemperatureLabel(from view: WeatherView) -> UILabel {
+        //WeatherView含まれるstackViewForImageViewAndLabelsを取得
+        let stackViewForImageViewAndLabels = getStackView(from: view)
+        //stackViewForImageViewAndLabelsに含まれるstackViewForLabelsを取得
+        let stackViewForLabels = getStackView(from: stackViewForImageViewAndLabels)
+        //stackViewForLabelsに初めに追加したUILabelを取得
+        let minTemperatureLabel = stackViewForLabels.subviews[0] as! UILabel
+        return minTemperatureLabel
     }
 }
